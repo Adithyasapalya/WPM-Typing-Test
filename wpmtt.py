3a8b48ebb4e1ee275dfecb82b8eabeedca7f7cc3
Adithya Sapalya <adithyasapalya30@gmail.com>
2026-01-09 21:40:26 +0530

    new function defined

--- wpmtt.py
+++ wpmtt.py
@@ -1,14 +1,22 @@
 import curses
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the Speed Typing Test!")
+    stdscr.addstr("\nPress any key to begin!")
+    stdscr.refresh()
+    stdscr.getkey()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_RED)
     curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
     stdscr.clear()
-    stdscr.addstr("Hello, World!")
+    stdscr.addstr(0, 0, "Hello, World!")
     stdscr.refresh()
-    stdscr.getkey()
+
+    start_screen(stdscr)
 
 wrapper(main)
 
