ce15cc82ecc0af3cfb9d5f1ad5fde4b706d99af8
Adithya Sapalya <adithyasapalya30@gmail.com>
2026-01-09 21:03:37 +0530

    added color to terminal

--- wpmtt.py
+++ wpmtt.py
@@ -2,9 +2,13 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_RED)
+    curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
     stdscr.clear()
     stdscr.addstr("Hello, World!")
     stdscr.refresh()
     stdscr.getkey()
 
 wrapper(main)
+
